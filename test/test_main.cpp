d37f9ba1980e337a2357ab961060a99b054dbfc0
goldholder999 <goldholder999@gmail.com>
2020-01-29 09:13:08 +0900

    test class create

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -9,6 +9,32 @@ TEST(AddTest2, CanPass) {
     EXPECT_EQ(-1, minus(0, 1));
 }
 
+class MyTestFixture : public ::testing::Test {
+public:
+    MyTestFixture() {
+
+    }
+    void SetUp()
+    {
+        printf("[%s:%d]\n", __FUNCTION__, __LINE__);
+    }
+
+    void TearDown()
+    {
+        printf("[%s:%d]\n", __FUNCTION__, __LINE__);
+    }
+
+    int sum(int a, int b)
+    {
+        return (a + b);
+    }
+};
+
+TEST_F(MyTestFixture, TestSum)
+{
+    ASSERT_GT(10, sum(1, 2));
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
